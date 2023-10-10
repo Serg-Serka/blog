d2793e67078a7245c8b59561cf47ed7a76462ecd
serg <sergij.stadnyk@gmail.com>
2023-10-11 01:57:24 +0300

    Added pagination for posts

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use App\Services\JsonPlaceholderService;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
 
 class BlogController extends Controller
@@ -17,7 +21,8 @@ class BlogController extends Controller
 
     public function index() : View
     {
-        $posts = $this->jsonPlaceholderService->getPosts();
+//        $posts = $this->jsonPlaceholderService->getPosts();
+        $posts = DB::table('posts')->paginate(15);
 
         return view('post.listing', ['posts' => $posts]);
     }
--- resources/views/post/listing.blade.php
+++ resources/views/post/listing.blade.php
@@ -7,23 +7,23 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            @foreach ($posts as $post)
+            @foreach ($posts->toArray()['data'] as $post)
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     <header>
                         <h2 class="text-lg font-medium text-gray-900">
-                            {{$post['title']}}
+                            {{$post->title}}
                         </h2>
                     </header>
 
                     <div>
                         <p class="mt-1 text-sm text-gray-600">
-                            {{$post['body']}}
+                            {{$post->body}}
                         </p>
                     </div>
                     <button
                         type="button"
-                        onclick="window.location = '{{ route("posts.show", $post['id']) }}'"
+                        onclick="window.location = '{{ route("posts.show", $post->id) }}'"
                         class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
                     >
                         View Post
@@ -31,6 +31,8 @@
                 </div>
             </div>
             @endforeach
+
+            {{$posts->render()}}
         </div>
     </div>
 </x-app-layout>
